4790d18f92538f64fc60663d98a763442f9eab1d
Danil notebook <makarovdanil289@gmail.com>
2023-02-03 22:47:37 +0500

    add ajax with php script formailer

---
+++ src/mailer/smart.php
@@ -0,0 +1,43 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+
+$mail->isSMTP();                                      
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = '';
+$mail->Password = '';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+ 
+$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->addAddress('makarovdanil289@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
